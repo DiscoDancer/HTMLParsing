03c3e9d128bdbc9d34c7c47031687c65ace23612
Vladislav Evtushenko <Vladislav.Evtushenko@euromoneyplc.com>
2021-02-20 13:37:11 +0300

    hello world

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using RestSharp;
+using RestSharp.Extensions;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static string UrlBase = "https://w1.dwar.ru/companion_info.php?nick=";
+
+        private static string[] Names = new[]
+        {
+            "KinGaude",
+        };
+
+        static void Main(string[] args)
+        {
+            var i = 0;
+            foreach (var name in Names)
+            {
+                var client = new RestClient(UrlBase + name);
+                var request = new RestRequest($"", Method.GET);
+                client.DownloadData(request).SaveAs($"{i++}.html");
+            }
+        }
+    }
+}
